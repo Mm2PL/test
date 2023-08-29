6713544a73bcc30c0bdc569392dd3890529aceaf
Mm2PL <miau@mail.kotmisia.pl>
2023-08-29 15:38:37 +0200

    test2

--- test.c
+++ test.c
@@ -2,3 +2,18 @@
 #define noval
 #define B (123)
 #define C (456)
+
+int afunc(void) {
+    int local = 0;
+
+    return 123;
+}
+int global=0;
+
+
+#define outside 0
+#define noval
+#define B (123)
+#define C (456)
+#undef noval
+#define D (789)
